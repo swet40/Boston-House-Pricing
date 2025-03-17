d4ead63ca60ccab4e31ee4df0185c9bed92c53c2
Sweta Pal <swetapal101112@gmail.com>
2025-03-17 12:56:41 +0530

    working

---
+++ app.py
@@ -0,0 +1,40 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaler=pickle.load(open('scale.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+
+def predict_api():
+    data=request.json['data']
+    print(data)
+    df_input = pd.DataFrame([data])
+    df_input['MEDV'] = 0 
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data_scaled = scaler.transform(df_input)
+    new_data_scaled = new_data_scaled[:, :-1]
+    output = regmodel.predict(new_data_scaled)
+    print(output[0])
+    return jsonify(output[0])
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text = "The House price prediction is {}".format(output))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
